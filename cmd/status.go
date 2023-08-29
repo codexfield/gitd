303cde11d5272204daf22a51ce79ce7eea63741a
thomas <thomasan1230@gmail.com>
2023-08-29 09:14:43 +0800

    fix: add retry for putobject failed

--- cmd/status.go
+++ cmd/status.go
@@ -4,6 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/go-git/go-git/v5"
@@ -30,7 +31,7 @@ var statusCmd = &cobra.Command{
 		}
 		head, err := r.Head()
 		if err != nil {
-			if err == plumbing.ErrReferenceNotFound {
+			if errors.Is(err, plumbing.ErrReferenceNotFound) {
 				head, err = r.Storer.Reference(plumbing.HEAD)
 				if err != nil {
 					fmt.Println("Get empty repo head error: ", err)
@@ -53,7 +54,7 @@ var statusCmd = &cobra.Command{
 		if s.IsClean() {
 			fmt.Println("nothing to commit, working tree clean")
 		} else {
-			fmt.Println("Changes not staged for commit:\n  (use \"git add <file>...\" to update what will be committed)\n  (use \"git restore <file>...\" to discard changes in working directory)\n")
+			fmt.Println("Changes not staged for commit:\n  (use \"git add <file>...\" to update what will be committed)")
 			fmt.Println("  " + s.String())
 		}
 	},
